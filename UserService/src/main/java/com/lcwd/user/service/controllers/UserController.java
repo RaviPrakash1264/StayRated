d0e6264a2989947d638b3a593cf96f04d117bf5e
Ravi Prakash <ravimehra1264@gmail.com>
2024-12-28 17:38:49 +0530

    added resilience4j and it's libraries

--- UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/lcwd/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.lcwd.user.service.controllers;
 
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +21,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-//    private Logger logger = LoggerFactory.getLogger(UserController.class);
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     //create
     @PostMapping
@@ -29,15 +32,15 @@ public class UserController {
 
     //single user get
 
-
+    int retryCount=1;
     @GetMapping("/{userId}")
 //    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
 //    @Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
-//    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
-//        logger.info("Get Single User Handler: UserController");
-//        logger.info("Retry count: {}", retryCount);
-
+        logger.info("Get Single User Handler: UserController");
+        logger.info("Retry count: {}", retryCount);
+        retryCount++;
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
@@ -45,14 +48,14 @@ public class UserController {
     //creating fall back  method for circuitbreaker
 
 
-//    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
-////        logger.info("Fallback is executed because service is down : ", ex.getMessage());
-//
-//        ex.printStackTrace();
-//
-//        User user = User.builder().email("dummy@gmail.com").name("Dummy").about("This user is created dummy because some service is down").userId("141234").build();
-//        return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
-//    }
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+//        logger.info("Fallback is executed because service is down : ", ex.getMessage());
+
+        ex.printStackTrace();
+
+        User user = User.builder().email("dummy@gmail.com").name("Dummy").about("This user is created dummy because some service is down").userId("141234").build();
+        return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
+    }
 
 
     //all user get
--- UserService/src/main/java/com/lcwd/user/service/entities/User.java
+++ UserService/src/main/java/com/lcwd/user/service/entities/User.java
@@ -31,7 +31,20 @@ public class User {
     @Transient
     private List<Rating> ratings=new ArrayList<>();
 
+    public User(){
 
+    }
+    private User(Builder builder) {
+        this.userId = builder.userId;
+        this.name = builder.name;
+        this.email = builder.email;
+        this.about = builder.about;
+        this.ratings = builder.ratings;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
     public String getUserId() {
         return userId;
     }
@@ -70,5 +83,43 @@ public class User {
 
     public void setRatings(List<Rating> ratings) {
         this.ratings = ratings;
+    }
+
+    public static class Builder {
+        private String userId;
+        private String name;
+        private String email;
+        private String about;
+        private List<Rating> ratings;
+
+        public Builder userId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+
+        public Builder about(String about) {
+            this.about = about;
+            return this;
+        }
+
+        public Builder ratings(List<Rating> ratings) {
+            this.ratings = ratings;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 }
