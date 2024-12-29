8d1b0d9319885a927718c93e0049bacd602dc2a7
Ravi Prakash <ravimehra1264@gmail.com>
2024-12-29 18:23:32 +0530

    added security for rating and hotel service

--- HotelService/src/main/java/com/lcwd/hotel/controllers/HotelController.java
+++ HotelService/src/main/java/com/lcwd/hotel/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.lcwd.hotel.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,7 +19,7 @@ public class HotelController {
 
     //create
 
-//    @PreAuthorize("hasAuthority('Admin')")
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
         Hotel hotel1=hotelService.create(hotel);
@@ -27,7 +28,7 @@ public class HotelController {
 
 
     //get single
-//    @PreAuthorize("hasAuthority('SCOPE_internal')")
+    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping("/{hotelId}")
     public ResponseEntity<Hotel> createHotel(@PathVariable String hotelId) {
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.get(hotelId));
@@ -35,7 +36,7 @@ public class HotelController {
 
 
     //get all
-//    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping
     public ResponseEntity<List<Hotel>> getAll(){
         return ResponseEntity.ok(hotelService.getAll());
